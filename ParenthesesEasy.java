ae6f47d7f924e7beb19feee793c220fb29de8980
cedricsabourin <164925880+cedricsabourin@users.noreply.github.com>
2025-03-14 11:03:32 -0400

    first problem to add to github

---
+++ ParenthesesEasy.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+
+public class ParenthesesEasy {
+    public static void main(String[] args) {
+        String case1 = "()";
+        String case2 = "()[]{}";
+        String case3 = "(]";
+        String case4 = "([])";
+        System.out.println(case1 + ":  " + isValid(case1));
+        System.out.println(case2 + ":  " + isValid(case2));
+        System.out.println(case3 + ":  " + isValid(case3));
+        System.out.println(case4 + ":  " + isValid(case4));
+    }
+
+    public static boolean isValid(String s) {
+        // Create a stack that will contain all the opening parentheses
+        Stack<Character> s1 = new Stack<>();
+
+        // loop through all characters of the string
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // add opening to stack
+            if (c == '(' || c == '{' || c == '[') {
+                s1.push(c);
+            } else {
+                // if it's a closing, we check if the most recent opening is the corresponding closing we found
+
+                char top = s1.pop();
+                if (!((top == '(' && c == ')')
+                 || (top == '{' && c == '}')
+                 || (top == '[' && c == ']'))) {
+                    return false;
+                 }
+            }
+        }
+
+        // if at the end of the algorithm the stack still has a parentheses, it's false because there are more opening than closing
+        if (!s1.isEmpty()) {
+            return false;
+        }
+
+        // if we reach here it means it's valid
+        return true;
+
+    }
+
+
+}
